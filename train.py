2311d8b0b71c1c0a63ab5bd6aebd0c5921615e1f
Allan Reyes <allan_bassail@hotmail.com>
2022-10-12 19:47:40 -0700

    Support training on a fixed experiment setting

--- train.py
+++ train.py
@@ -5,11 +5,9 @@ from agents import make_agent
 from envs import make_env
 
 
-def _main(agent_type: str, timesteps: int, n_frames: int, n_envs: int) -> None:
-    print(f"Training RL agent {agent_type} for {timesteps} timesteps using {n_frames} frames and {n_envs} envs")
-
+def _main(agent_type: str, timesteps: int, n_frames: int, n_envs: int, setting: int) -> None:
     seed = 24
-    env = make_env(n_frames, n_envs, seed)
+    env = make_env(n_frames, n_envs, setting, seed)
 
     model = make_agent(agent_type, env=env, seed=seed)
 
@@ -21,9 +19,11 @@ if __name__ == "__main__":
     parser = argparse.ArgumentParser(description="Trains RL agents that represent the subordinate subject")
     parser.add_argument("-a", type=str, required=True, help="The type of the agent to use")
     parser.add_argument("-t", type=int, default=500, help="The total training timesteps. Defaults to 500")
-    parser.add_argument("--n-frames", type=int, default=1, help="Number of frames to stack as input to the agent")
-    parser.add_argument("--n-envs", type=int, default=1, help="Number of concurrent environments to use for training")
+    parser.add_argument("--n-frames", type=int, default=1, help="Frames to stack as input to the agent. Defaults to 1")
+    parser.add_argument("--n-envs", type=int, default=1, help="Environments to use for training. Defaults to 1")
+    parser.add_argument("--setting", type=int, default=None, help="Experiment setting to train on. Defaults to None")
 
     args = parser.parse_args()
 
-    _main(args.a, args.t, args.n_frames, args.n_envs)
+    print(f"Training RL agent using the following args: {args}")
+    _main(args.a, args.t, args.n_frames, args.n_envs, args.setting)
